6876c0ab3db855bc0513b80a15fe15a467e1a751
Jonas Syrstad <Jonas.Syrstad@dnv.com>
2024-03-05 08:53:23 +0100

    added support for more verbs

--- Stardust.Interstellar.Rest.Annotations/Attributes/GetAttribute.cs
+++ Stardust.Interstellar.Rest.Annotations/Attributes/GetAttribute.cs
@@ -17,4 +17,67 @@ namespace Stardust.Interstellar.Rest.Annotations
 	    {
 	    }
 	}
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+    public class LockAttribute : VerbAttribute
+    {
+        public LockAttribute() : base("LOCK")
+        {
+        }
+
+        public LockAttribute(string route) : base("LOCK", route)
+        {
+        }
+
+        public LockAttribute(string route, string serviceDescription) : base("LOCK", route, serviceDescription)
+        {
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+    public class UnLockAttribute : VerbAttribute
+    {
+        public UnLockAttribute() : base("UNLOCK")
+        {
+        }
+
+        public UnLockAttribute(string route) : base("UNLOCK", route)
+        {
+        }
+
+        public UnLockAttribute(string route, string serviceDescription) : base("UNLOCK", route, serviceDescription)
+        {
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+    public class MoveAttribute : VerbAttribute
+    {
+        public MoveAttribute() : base("MOVE")
+        {
+        }
+
+        public MoveAttribute(string route) : base("MOVE", route)
+        {
+        }
+
+        public MoveAttribute(string route, string serviceDescription) : base("MOVE", route, serviceDescription)
+        {
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+    public class CopyAttribute : VerbAttribute
+    {
+        public CopyAttribute() : base("COPY")
+        {
+        }
+
+        public CopyAttribute(string route) : base("COPY", route)
+        {
+        }
+
+        public CopyAttribute(string route, string serviceDescription) : base("COPY", route, serviceDescription)
+        {
+        }
+    }
 }
